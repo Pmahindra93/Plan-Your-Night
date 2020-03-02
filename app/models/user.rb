026245de1c6c44f58ac02de27ab0c16eea7c7cca
Bjarne Hinkel <bjarne@hinkels.de>
2020-03-02 17:03:39 +0100

    added gem and relation in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :favourites
+  has_many :nights
+  has_many :venue_reviews
 end
