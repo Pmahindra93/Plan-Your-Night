f833a8de05b1fe21c3a6a5d9f7a5bf819ea62ae1
Majd Sufian <majdnewsufian@gmail.com>
2020-03-03 12:01:21 +0100

    done with frontend of home page

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ gem 'pg', '~> 0.21'
 gem 'puma'
 gem 'rails', '5.2.4.1'
 gem 'redis'
-
+gem 'bootstrap-sass'
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.12.0'
 gem 'sassc-rails'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,28 @@ class PagesController < ApplicationController
 
   def home
   end
+  def search
+    query_name = 'name ILIKE ?'
+    query_brand = 'brand ILIKE ?'
+    query_both = 'brand ILIKE ? AND name ILIKE ?'
+    unless params[:search].nil?
+      location = params[:search][:location]
+      category = params[:search][:category]
+      budget = params[:search][:budget]
+
+      if (params[:search][:location] == "" && name == "" && budget == "")
+        @venues = Venue.all
+      elsif params[:search][:location] != "" && name == ""
+        @listings = Listing.where(query_brand, "%#{brand}%")
+      elsif params[:search][:brand] == "" && name != ""
+        @listings = Listing.where(query_name, "%#{name}%")
+      else
+        @listings = Listing.where(query_both, "%#{brand}%", "%#{name}%")
+      end
+    else
+      @listings = Listing.all
+    end
+  end
 end
+
+
