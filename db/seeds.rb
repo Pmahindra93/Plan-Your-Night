fc718e63f6b7b4fd8e60b37bfca03fd0efef357d
Bjarne Hinkel <bjarne@hinkels.de>
2020-03-03 11:20:19 +0100

    fixing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ Venue.delete_all
 User.delete_all
 
 solar = Venue.new(
-  type: 'bar',
+  venue_type: 'bar',
   category: 'retro',
   name: 'solar bar',
   address: 'Stresemannstrasse 76, Berlin',
@@ -22,15 +22,14 @@ solar = Venue.new(
   description: 'Unser einmaliger Außenfahrstuhl führt aus dem Eingangsbereich im Erdgeschoss direkt ins Restaurant in 70 Meter Höhe im Himmel über Berlin. Ein spektakuläres 270-Grad-Panorama trifft bei uns auf gemütliches und urbanes Design. Von jedem Platz aus beobachtet man den wunderschönen Sonnenuntergang und das nächtliche, bunt strahlende Lichtermeer der Hauptstadt.'
 )
 
-solar.user = User.new(email: 'bjarne@hinkels.de', first_name: 'Bjarne', last_name: 'Hinkel')
 solar1 = URI.open('https://res.cloudinary.com/bjarnehinkel/image/upload/v1583229290/solar_mdtetd.jpg')
 solar2 = URI.open('https://res.cloudinary.com/bjarnehinkel/image/upload/v1583229342/solar2_uasmi8.jpg')
-solar.photo.attach(io: solar1, filename: 'solar.jpg', content_type: 'image/jpg')
-solar.photo.attach(io: solar2, filename: 'solar1.jpg', content_type: 'image/jpg')
+solar.photos.attach(io: solar1, filename: 'solar.jpg', content_type: 'image/jpg')
+solar.photos.attach(io: solar2, filename: 'solar1.jpg', content_type: 'image/jpg')
 solar.save
 
 matrix = Venue.new(
-  type: 'club',
+  venue_type: 'club',
   category: 'modern',
   name: 'matrix',
   address: 'Charlottenstraße 34, Berlin',
@@ -42,9 +41,8 @@ matrix = Venue.new(
   description: 'Unser einmaliger Außenfahrstuhl führt aus dem Eingangsbereich im Erdgeschoss direkt ins Restaurant in 70 Meter Höhe im Himmel über Berlin. Ein spektakuläres 270-Grad-Panorama trifft bei uns auf gemütliches und urbanes Design. Von jedem Platz aus beobachtet man den wunderschönen Sonnenuntergang und das nächtliche, bunt strahlende Lichtermeer der Hauptstadt.'
 )
 
-solar.user = User.new(email: 'bjarne@hinkels.de', first_name: 'Bjarne', last_name: 'Hinkel')
 matrix1 = URI.open('https://res.cloudinary.com/bjarnehinkel/image/upload/v1583229930/matrix1_weg4lx.jpg')
 matrix2 = URI.open('https://res.cloudinary.com/bjarnehinkel/image/upload/v1583229933/matrix2_zcsote.jpg')
-matrix.photo.attach(io: matrix1, filename: 'matrix1.jpg', content_type: 'image/jpg')
-matrix.photo.attach(io: matrix2, filename: 'matrix2.jpg', content_type: 'image/jpg')
+matrix.photos.attach(io: matrix1, filename: 'matrix1.jpg', content_type: 'image/jpg')
+matrix.photos.attach(io: matrix2, filename: 'matrix2.jpg', content_type: 'image/jpg')
 matrix.save
