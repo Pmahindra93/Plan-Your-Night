70e9be6177eb92a76a69db9db1f19ea4dd645a74
Majd Sufian <majdnewsufian@gmail.com>
2020-03-03 15:06:40 +0100

    finishing the search button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,24 +3,5 @@ class PagesController < ApplicationController
 
   def home
   end
-  def search
-    # query_location = 'location ILIKE ?'
-    # query_brand = 'brand ILIKE ?'
-    # query_both = 'brand ILIKE ? AND name ILIKE ?'
-    unless params[:search].nil?
-      location = params[:search][:location]
-      category = params[:search][:category]
-      budget = params[:search][:budget]
 
-      if (location == "Berlin" && category == "" && budget != "")
-        @venues = Venue.all
-      # elsif location == "Berlin" && category == "both" && budget == ""
-      #   @venues = Venue.all
-      elsif location == "Berlin" && category != "" && budget != ""
-        @venues = Venue.where(params[:category])
-      end
-    else
-      @venues = Venue.all
-    end
-  end
 end
