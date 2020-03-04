0c7fc8336bfc97d8b9129752f2bc619228f4db53
Luis Caballero <mendoza386@gmail.com>
2020-03-04 15:23:53 +0100

    routes actualized

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,20 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  root to: 'nights#new'
-   get 'users/:id', to: 'users#show', as: :user
+  root to: ‘nights#new’
+  get ‘users/:id’, to: ‘users#show’, as: :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
   resources :nights, only: [:new, :create] do
-<<<<<<< HEAD
-     resources :venues, only: [:index, :show]
-
+    resources :venues, only: [:index, :show] do
+      collection do
+        get 'clubs'
+      end
+    end
   end
-  # edit and only outside everything
-  resources :venues do
-    resources :favourites, only: :create
-       # resources for :venue_reviews inside venues
-    resources :venue_reviews, only: :create
-=======
-    resources :venues, only: [:index, :show]
->>>>>>> master
-  end
-  resources :venue_reviews, only: [:edit, :update]
 
   resources :venues, only: [] do
     resources :favourites, only: :create
+    resources :venue_reviews, only: :create
   end
+  resources :venue_reviews, only: [:edit, :update]
 end
