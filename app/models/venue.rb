85fe7ea0e823c177c9a330f550a45b598d7330ac
Luis Caballero <mendoza386@gmail.com>
2020-03-04 15:28:57 +0100

    merge probleme venue and routes solved

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,9 +1,6 @@
 class Venue < ApplicationRecord
   has_many_attached :photos
-<<<<<<< HEAD
   has_many :reviews, dependent: :destroy
-=======
   has_many :night_venues
   has_many :nights, :through => :night_venues
->>>>>>> master
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: ‘nights#new’
-  get ‘users/:id’, to: ‘users#show’, as: :user
+  root to: 'nights#new'
+  get 'users/:id', to: 'users#show', as: :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :nights, only: [:new, :create] do
     resources :venues, only: [:index, :show] do
