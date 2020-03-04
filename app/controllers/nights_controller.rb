0167921499b5b94089a98a7e098c39879e80e5c8
Majd Sufian <majdnewsufian@gmail.com>
2020-03-04 11:52:34 +0100

    refactor the buttons in show page

--- app/controllers/nights_controller.rb
+++ app/controllers/nights_controller.rb
@@ -1,32 +1,32 @@
 class NightsController < ApplicationController
-  def index
-    @user = current_user
-    @nights = @user.nights
-  end
+  # def index
+  #   @user = current_user
+  #   @nights = @user.nights
+  # end
 
-  def new
-    @nights = Night.new
-    @venue = Venue.find(params[:venue_id])
-  end
+  # def new
+  #   @nights = Night.new
+  #   @venue = Venue.find(params[:venue_id])
+  # end
 
-  def create
-    @night = Night.new(night_params)
-    @user = current_user
-    @night.user = @user
-    @venue = Venue.find(params[:venue_id])
-    @night.venue = @venue
-    # @night.total_price_segment = @night.days * @night.listing.price if @night.days
-    if @night.save
-      redirect_to user_path(@user)
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @night = Night.new(night_params)
+  #   @user = current_user
+  #   @night.user = @user
+  #   @venue = Venue.find(params[:venue_id])
+  #   @night.venue = @venue
+  #   # @night.total_price_segment = @night.days * @night.listing.price if @night.days
+  #   if @night.save
+  #     redirect_to user_path(@user)
+  #   else
+  #     render :new
+  #   end
+  # end
 
 
-  private
+  # private
 
-  def night_params
-    params.require(:night).permit(:total_price_segment)
-  end
+  # def night_params
+  #   params.require(:night).permit(:total_price_segment)
+  # end
 end
