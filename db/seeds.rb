ebe56ee089e15acd71775f899b4fd6bae452ebc5
Bjarne Hinkel <bjarne@hinkels.de>
2020-03-06 13:01:33 +0100

    seed now works completely

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,8 @@ LOCATION = "Berlin"
 CLIENT_ID = ENV['CLIENT_ID']
 CLIENT_SECRET = ENV['CLIENT_SECRET']
 V = '20180323'
-core = "https://api.foursquare.com/v2/venues/explore?client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=#{V}&limit=5&near=#{LOCATION}&categoryId="
+number_of_each = 5
+core = "https://api.foursquare.com/v2/venues/explore?client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=#{V}&limit=#{number_of_each}&near=#{LOCATION}&categoryId="
 
 bar_id = '4bf58dd8d48988d116941735'
 bar_url = "#{core}#{bar_id}"
@@ -28,6 +29,25 @@ def price_seg_gen(info)
   return price_segment.join
 end
 
+def credit_card_check(info)
+  credit_card_eval = info["response"]["venue"]["attributes"]["groups"][1]["items"][0]["displayValue"]
+  if credit_card_eval == "Nein"
+    return false
+  elsif credit_card_eval == "Ja"
+    return true
+  else
+    return [true, false].sample
+  end
+end
+
+def club_opening(info)
+  unless info["response"]["venue"]["hours"].nil?
+    return "#{info["response"]["venue"]["hours"]["timeframes"][0]["open"][0]["renderedTime"]}"
+  else
+    return "Opeining Times Unknown"
+  end
+end
+
 bars["response"]["groups"][0]["items"].each do |item|
   info_url = open("https://api.foursquare.com/v2/venues/#{item["venue"]["id"]}?client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}&v=#{V}").read
   info = JSON.parse(info_url)
@@ -38,13 +58,13 @@ bars["response"]["groups"][0]["items"].each do |item|
     address: "#{info["response"]["venue"]["location"]["formattedAddress"][0]}, #{info["response"]["venue"]["location"]["formattedAddress"][1]}",
     opening_hours: "#{info["response"]["venue"]["popular"]["timeframes"][0]["open"][0]["renderedTime"]}",
     price_segment: price_seg_gen(info),
-    card_accepted: [true, false].sample,
+    card_accepted: credit_card_check(info),
     description: "#{info["response"]["venue"]["tips"]["groups"][0]["items"][0]["text"]}"
   )
-  prefix = info["response"]["venue"]["photos"]["groups"]["items"]["prefix"]
-  width = info["response"]["venue"]["photos"]["groups"]["items"]["width"]
-  height = info["response"]["venue"]["photos"]["groups"]["items"]["height"]
-  suffix = info["response"]["venue"]["photos"]["groups"]["items"]["suffix"]
+  prefix = info["response"]["venue"]["photos"]["groups"][0]["items"][0]["prefix"]
+  width = info["response"]["venue"]["photos"]["groups"][0]["items"][0]["width"]
+  height = info["response"]["venue"]["photos"]["groups"][0]["items"][0]["height"]
+  suffix = info["response"]["venue"]["photos"]["groups"][0]["items"][0]["suffix"]
   file = URI.open("#{prefix}#{width}x#{height}#{suffix}")
   bar.photos.attach(io: file, filename: "#{info["response"]["venue"]["name"]}.jpg", content_type: 'image/jpg')
   bar.save!
@@ -61,15 +81,15 @@ clubs["response"]["groups"][0]["items"].each do |item|
     category: ['Modern','Retro','Alternative','Adult','High-End','Casual'].sample,
     name: info["response"]["venue"]["name"],
     address: "#{info["response"]["venue"]["location"]["formattedAddress"][0]}, #{info["response"]["venue"]["location"]["formattedAddress"][1]}",
-    opening_hours: "#{info["response"]["venue"]["popular"]["timeframes"][0]["open"][0]["renderedTime"]}",
+    opening_hours: club_opening(info),
     price_segment: price_seg_gen(info),
-    card_accepted: [true, false].sample,
+    card_accepted: credit_card_check(info),
     description: "#{info["response"]["venue"]["tips"]["groups"][0]["items"][0]["text"]}"
   )
-  prefix = info["response"]["venue"]["photos"]["groups"]["items"]["prefix"]
-  width = info["response"]["venue"]["photos"]["groups"]["items"]["width"]
-  height = info["response"]["venue"]["photos"]["groups"]["items"]["height"]
-  suffix = info["response"]["venue"]["photos"]["groups"]["items"]["suffix"]
+  prefix = info["response"]["venue"]["photos"]["groups"][0]["items"][0]["prefix"]
+  width = info["response"]["venue"]["photos"]["groups"][0]["items"][0]["width"]
+  height = info["response"]["venue"]["photos"]["groups"][0]["items"][0]["height"]
+  suffix = info["response"]["venue"]["photos"]["groups"][0]["items"][0]["suffix"]
   file = URI.open("#{prefix}#{width}x#{height}#{suffix}")
   club.photos.attach(io: file, filename: "#{info["response"]["venue"]["name"]}.jpg", content_type: 'image/jpg')
   club.save!
