e557dafc1f9865ebf20dab5f6a599066461e38c8
Bjarne Hinkel <bjarne@hinkels.de>
2020-03-09 11:04:54 +0100

    channged controller

--- app/models/night.rb
+++ app/models/night.rb
@@ -3,5 +3,4 @@ class Night < ApplicationRecord
   has_many :night_venues, dependent: :destroy
   has_many :venues, through: :night_venues
   validates :location, presence: true
-  validates_associated :venues, uniqueness: true
 end
