34df0e1c29dd75c52b38a73f8421f6d6d3539181
Bjarne Hinkel <bjarne@hinkels.de>
2020-03-09 10:21:38 +0100

    added a validation on nights

--- app/models/night.rb
+++ app/models/night.rb
@@ -3,6 +3,5 @@ class Night < ApplicationRecord
   has_many :night_venues, dependent: :destroy
   has_many :venues, through: :night_venues
   validates :location, presence: true
-  # validates :category, presence: true
-  # validates :budget, presence: true
+  validates_associated :venues, uniqueness: true
 end
